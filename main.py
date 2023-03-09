54e5e7f57d9a5c25e5529120343ca7bf64225870
tongticktack <provemyself99@gmail.com>
2023-03-09 10:28:01 +0000

    assignment02

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
+    result = []
+    for e in range(0, len(nums)):
+        dif = target - nums[e]
+        for ee in range(e + 1, len(nums)):
+            if(nums[ee] == dif):
+                result.append(e)
+                result.append(ee)
+                return result
+
+print("result : ", twoSum([2,7,11,15], 13))
+
